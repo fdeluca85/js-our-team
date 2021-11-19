58ff2c15f698a0b92a2f20aec51a344a5d28027e
francesca <fradeluca85@gmail.com>
2021-11-19 16:13:14 +0100

    array membri team

--- script.js
+++ script.js
@@ -2,3 +2,41 @@
 // Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // Prendendo come riferimento il layout di esempio presente nell'html, stampiamo tutte le card del nostro team.
 // BONUS: Utilizziamo poi gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team.
+
+// alert("ciao");
+
+
+// array dei membri del team
+const team = [
+    {
+        'name': 'Wayne Barnett', 
+        'role': 'Founder & CEO',
+        'image': 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image': 'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'img/walter-gordon-office-manager.jpg'
+    },
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'img/scott-estrada-developer.jpg'
+    },
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'img/barbara-ramos-graphic-designer.jpg'
+    },
+];
+console.log(team);
